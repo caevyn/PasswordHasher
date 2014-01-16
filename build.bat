5eaa532cdf7341431336e5e9d6d3cdcbd50be943
Matt Murphy <m.murphy@nib.com.au>
2014-01-16 12:46:06 +1100

    fix build.bat to point to nuget.exe

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 cls
 
-"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "tools" "-ExcludeVersion"
+"src\.nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
+"src\.nuget\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "tools" "-ExcludeVersion"
 "tools\FAKE\tools\Fake.exe" build.fsx
 
 pause
